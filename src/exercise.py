d9a2547fa16590eba1868e7cb7aec9d596d36739
Jake Lee <jake.lee@fdmgroup.com>
2021-10-18 19:03:07 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,4 +1,5 @@
 from person import Person
+import csv
 
 def main():
     file = input("filename:")
@@ -7,12 +8,20 @@ def main():
     print("persons: " + str(len(records)))
     print("persons:")
     for person in records:
-        print(person)
+        print(person.get_name())
 
 def read_records_from_file(file):
     records = []
     # write here the code for reading from file
     # and printing the read records
+    try:
+        f = open(file, "r")
+        reader = csv.reader(f, delimiter = ",")
+        for row in reader:
+            records.append(Person(row[0], int(row[1])))
+    except:
+        print("Error")
+    return records
 
 if __name__ == '__main__':
     main()
